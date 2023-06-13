28ad3681c022923658f0d58b7d05b82096a8b11e
Nikolay <nik86olay@gmail.com>
2023-06-13 22:36:12 +0300

    securityMethInit commit

--- src/main/java/com/example/hibernate/Controller.java
+++ src/main/java/com/example/hibernate/Controller.java
@@ -3,6 +3,9 @@ package com.example.hibernate;
 import com.example.hibernate.Repository.PersonRepository;
 import com.example.hibernate.model.Person;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasRole;
 
 @RestController
 @AllArgsConstructor
@@ -21,18 +27,21 @@ public class Controller {
     PersonRepository repository;
 
     @Transactional
+    @PostAuthorize("hasRole ('DELETE') or hasRole('WRITE')")
     @GetMapping("/persons/by-city")
     public List<Person> Profile(@RequestParam String city) {
         return repository.findPersonByCityOfLiving(city);
     }
 
     @Transactional
+    @Secured({"ROLE_READ"})
     @GetMapping("/persons/by-age")
     public List<Person> Profile(@RequestParam int age) {
         return repository.findPersonByAgeLessThanOrderByAge(age);
     }
 
     @Transactional
+    @PreAuthorize("#name == authentication.principal.username")
     @GetMapping("/persons/by-nameAndSurname")
     public Optional<Person> Profile(@RequestParam String name, @RequestParam String surname) {
         return repository.findPersonByNameAndSurname(name, surname);
@@ -40,6 +49,7 @@ public class Controller {
 
     @Transactional
     @GetMapping("/persons/by-all")
+    @RolesAllowed({"ROLE_WRITE"})
     public String ByAll() {
         return "Hello all";
     }
--- src/main/java/com/example/hibernate/security/SecurityConfiguration.java
+++ src/main/java/com/example/hibernate/security/SecurityConfiguration.java
@@ -3,31 +3,29 @@ package com.example.hibernate.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("user").password(passwordEncoder().encode("password")).roles("USER");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().and()
-                .authorizeRequests().antMatchers("/persons/by-all").permitAll()
+                .withUser("Ivan").password(passwordEncoder().encode("password")).roles("READ")
                 .and()
-                .authorizeRequests().anyRequest().authenticated();
+                .withUser("Lex").password(passwordEncoder().encode("password1")).roles("WRITE")
+                .and()
+                .withUser("Roma").password(passwordEncoder().encode("password2")).roles("READ", "DELETE");
     }
 
 }
